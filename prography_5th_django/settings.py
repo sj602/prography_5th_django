e325e74e7108472afd23245f0e42dd6a2e40789a
sj602 <tjswls602@gmail.com>
2019-09-01 15:41:32 +0900

    modify setting

--- prography_5th_django/settings.py
+++ prography_5th_django/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@hl!pf#sf=s)q%%8r6ke0m_9-p*&&ymq-#58jlmhni^t8&^t3c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
